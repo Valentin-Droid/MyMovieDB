616d85fb4e1a87386f40ee989941b394086f4f04
valentin-droid <valentin.drelon23@icloud.com>
2023-02-15 14:26:20 +0100

    Initial Commit

---
+++ MyMovieDB/MyMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieDBApp.swift
+//  MyMovieDB
+//
+//  Created by Valentin Drelon on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
